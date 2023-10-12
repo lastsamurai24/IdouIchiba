b438551a00b6a2d6202bb717a33bc135eff46033
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-12 16:19:20 +0900

    cimmit

--- database_utils.py
+++ database_utils.py
@@ -5,7 +5,7 @@ DATABASE_PATH = 'mydatabase.db'
 def get_products_by_category(category):
     conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
-    cursor.execute("SELECT product_name, price FROM products WHERE category=?", (category,))
+    cursor.execute("SELECT product_name, price FROM products WHERE category LIKE ?", ('%' + category + '%',))
     products = cursor.fetchall()
     conn.close()
     return products
