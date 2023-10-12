90128cf484d2e7d82e7b3394ae729408f1c9d071
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-12 22:25:32 +0900

    cn3

--- app.py
+++ app.py
@@ -50,16 +50,24 @@ def handle_message_combined(event):
     received_msg = event.message.text
     
     # First, perform the original handle_message process
-    products = get_products_by_category(received_msg)
-    if products:
-        reply_msg = "\n".join([f"{product[0]}: {product[1]}" for product in products])
+    products_by_category = get_products_by_category(received_msg)
+    
+    if products_by_category:
+        reply_msg = "\n".join([f"{product[0]}: {product[1]}" for product in products_by_category])
     else:
         # If no products found with the original method, try handle_message_updated process
-        exact_products = get_products_by_partial_category(received_msg)
-        if any(product[0] == received_msg for product in exact_products):
+        products_by_exact_name = get_products_by_partial_category(received_msg)
+        if any(product[0] == received_msg for product in products_by_exact_name):
             reply_msg = f"いくつ{received_msg}を買いますか？"
         else:
             reply_msg = "製品が見つかりませんでした。"
+    
+    # Send the reply message
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
+
+# Displaying the combined function for reference
+handle_message_combined
+
 
 log_handler = RotatingFileHandler("flask_app.log", maxBytes=10000, backupCount=3)
 log_handler.setLevel(logging.INFO)
@@ -69,4 +77,3 @@ app.logger.addHandler(log_handler)
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-    
