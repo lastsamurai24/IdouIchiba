78205da7a44726ed411e576505f1d7e6a2cfbc58
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-13 15:10:01 +0900

    cm02

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from dotenv import load_dotenv
 from database_utils import get_products_by_category
 from database_utils import get_products_by_partial_category
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage,
-                            ButtonsTemplate, TemplateSendMessage, PostbackAction, URIAction)
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    ButtonsTemplate,
+    TemplateSendMessage,
+    PostbackAction,
+    URIAction,
+)
 
 load_dotenv()
 
@@ -49,54 +56,50 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-
-
 def handle_message_combined(event):
     received_msg = event.message.text
-    
+
     if received_msg.isdigit():
-        # handle_quantity_message関数を呼び出す際に、received_msg引数を追加
-        reply_msg = handle_quantity_message(event, int(received_msg), received_msg)
+        # ここで、前回のメッセージ（商品名）を取得するロジックが必要です。
+        # 例えば、セッションやデータベースを使用して前回のメッセージを保存しておくなどの方法が考えられます。
+        previous_msg = "前回のメッセージを取得するロジック"
+        reply = handle_quantity_message(event, int(received_msg), previous_msg)
     else:
-        
         products_by_category = get_products_by_category(received_msg)
-        
+
         if products_by_category:
             reply_msg = "\n".join([f"{product[0]}: {product[1]}" for product in products_by_category])
+            reply = TextSendMessage(text=reply_msg)
         else:
-           
             products_by_exact_name = get_products_by_partial_category(received_msg)
             if any(product[0] == received_msg for product in products_by_exact_name):
-                reply_msg = f"いくつ{received_msg}を買いますか？"
+                # ここで、商品名をhandle_quantity_message関数に渡します。
+                reply = handle_quantity_message(event, 1, received_msg)  # 1はデフォルトの数量として設定しています。
             else:
                 reply_msg = "製品が見つかりませんでした。"
-    
-
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
-
-
+                reply = TextSendMessage(text=reply_msg)
+    if isinstance(reply, TemplateSendMessage):
+        line_bot_api.reply_message(event.reply_token, reply)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply.text))
 def handle_quantity_message(event, quantity, received_msg):
     # ボタンテンプレートの作成
     buttons_template = ButtonsTemplate(
         thumbnail_image_url="https://example.com/bot/images/image.jpg",
-        title="Menu",
+        title="選択した商品",
         text=f"{received_msg}を{quantity}つでよろしいでしょうか？",
         actions=[
             PostbackAction(label="買う", data=f"action=buy&quantity={quantity}"),
             PostbackAction(label="カートにいれる", data=f"action=add&quantity={quantity}"),
-            URIAction(label="商品の詳細", uri="http://example.com/page/123")
-        ]
+            URIAction(label="商品の詳細", uri="http://example.com/page/123"),
+        ],
     )
 
     # テンプレートメッセージの作成
-    template_message = TemplateSendMessage(
-        alt_text=f"{quantity}でよろしいでしょうか？",
-        template=buttons_template
-    )
+    template_message = TemplateSendMessage(alt_text=f"{quantity}でよろしいでしょうか？", template=buttons_template)
 
     # テンプレートメッセージを返す
     line_bot_api.reply_message(event.reply_token, template_message)
-
 
 
 log_handler = RotatingFileHandler("flask_app.log", maxBytes=10000, backupCount=3)
