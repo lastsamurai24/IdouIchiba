5c72201975d0ab9d6e421756ecb399ce9a9595da
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-13 16:38:57 +0900

    cm4

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def handle_quantity_message(event, quantity, received_msg):
         actions=[
             PostbackAction(label="買う", data=f"action=buy&quantity={quantity}"),
             PostbackAction(label="カートにいれる", data=f"action=add&quantity={quantity}"),
-            URIAction(label="商品の詳細", uri="https://www.rakuten.co.jp/"),
+            URIAction(label="商品の詳細", uri="https://127.0.0.1:5000/supermarket.onrender.com"),
         ],
     )
 
